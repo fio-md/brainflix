0b88c14e9893f9c545003c31afed4e86a1f4014f
Fiorella Marky <fiorella.marky@gmail.com>
2024-10-23 19:37:39 -0400

    complete mobile styling

--- src/components/ActiveVideo/ActiveVideo.jsx
+++ src/components/ActiveVideo/ActiveVideo.jsx
@@ -1,35 +1,30 @@
 import Button from "../Button/Button";
 import Avatar from "../Avatar/Avatar";
 import CommentItem from "../CommentItem/CommentItem";
+import VideoInfo from "../VideoInfo/VideoInfo";
 import "./ActiveVideo.scss";
 import commentIcon from "/src/assets/images/icons/add_comment.svg";
+import avatarPic from "/src/assets/images/Mohan-muruge.jpg";
 
 const ActiveVideo = ({ activeVideo }) => {
   return (
     <>
-      <section className="info">
-        <h1 className="info__title">{activeVideo.title}</h1>
-        <div className="info__summary">
-          <h4 className="info__channel">{`By ${activeVideo.channel}`}</h4>
-          <span className="info__date">{activeVideo.timestamp}</span>
-          <div className="info__views">{activeVideo.views}</div>
-          <div className="info__likes">{activeVideo.likes}</div>
-        </div>
-        <p className="info__description">{activeVideo.description}</p>
-      </section>
+      <VideoInfo activeVideo={activeVideo} />
       <section className="comments">
         <form action="submit" className="form">
-          <Avatar />
-          <label htmlFor="comment" className="form__label">
-            join the conversation
-          </label>
-          <textarea
-            className="form__comment"
-            name="comment"
-            id="comment"
-            placeholder="Add a new comment"
-          ></textarea>
-          <Button buttonIcon={commentIcon} buttonText="comment" />
+          <Avatar picture={avatarPic} />
+          <div className="form__main">
+            <label htmlFor="comment" className="form__label">
+              join the conversation
+            </label>
+            <textarea
+              className="form__comment"
+              name="comment"
+              id="comment"
+              placeholder="Add a new comment"
+            ></textarea>
+            <Button buttonIcon={commentIcon} buttonText="comment" />
+          </div>
         </form>
         <ul className="comments__list">
           {activeVideo.comments.map((comment) => {
---
+++ src/components/VideoInfo/VideoInfo.jsx
@@ -0,0 +1,23 @@
+import "./VideoInfo.scss";
+
+const VideoInfo = ({ activeVideo }) => {
+  return (
+    <section className="info">
+      <h1 className="info__title">{activeVideo.title}</h1>
+      <div className="info__summary">
+        <div className="info__summary-left">
+          <h4 className="info__channel">{`By ${activeVideo.channel}`}</h4>
+          <span className="info__date">{activeVideo.timestamp}</span>
+        </div>
+        <div className="info__summary-right">
+          <div className="info__views">{activeVideo.views}</div>
+          <div className="info__likes">{activeVideo.likes}</div>
+        </div>
+      </div>
+      <p className="info__description">{activeVideo.description}</p>
+      <span className="info__comment-count">{activeVideo.comments.length} comments</span>
+    </section>
+  );
+};
+
+export default VideoInfo;
